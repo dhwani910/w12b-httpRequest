76951dad162542c0629edd2b8c80b3507a206c6e
unknown <pateldhwani59@gmail.com>
2020-12-08 23:57:53 -0800

    implement httprequest

---
+++ js/app.js
@@ -0,0 +1,84 @@
+//delete comments in all files and delete template files
+
+function sendTweet() {
+    let tweetTitle = document.getElementById("title-input");
+    let tweetBody = document.getElementById("body-input");
+    let tweetData = {
+        title: tweetTitle,
+        body: tweetBody,
+        userId: 1
+    }
+   
+    
+    let ajax = new XMLHttpRequest();
+    ajax.onreadystatechange = function() {
+        if(this.readyState == 4 && this.status == 201) {
+            console.log(JSON.parse(this.responseText));
+            alert('your tweet have been posted successfully')
+        }
+    }
+    ajax.open("POST", "https://jsonplaceholder.typicode.com/posts", true);
+    ajax.setRequestHeader("Content-Type", "application/json");
+    ajax.send(JSON.stringify(tweetData));
+}
+let tweetButton = document.getElementById("tweet-submit");
+tweetButton.addEventListener("click", sendTweet);
+
+
+
+function updateTweet() {
+    let tweetTitle = document.getElementById("title-input");
+    let tweetBody = document.getElementById("body-input");
+    let tweetData = {
+        title: tweetTitle,
+        body: tweetBody,
+        userId: 1
+    }
+    let ajax = new XMLHttpRequest();
+    ajax.onreadystatechange = function() {
+        if(this.readyState == 4 && this.status == 200) {
+            console.log(JSON.parse(this.responseText));
+        }
+    }
+    ajax.open("PATCH", "https://jsonplaceholder.typicode.com/posts/1", true);
+    ajax.setRequestHeader("Content-Type", "application/json");
+    ajax.send(JSON.stringify(tweetData));
+}
+updateTweet();
+
+
+
+function deleteTweet() {
+    let ajax = new XMLHttpRequest();
+    ajax.onreadystatechange = function() {
+        if(this.readyState == 4 && this.status == 200) {
+            console.log(JSON.parse(this.responseText));
+        }
+    }
+    ajax.open("DELETE", "https://jsonplaceholder.typicode.com/posts/1", true);
+    ajax.setRequestHeader("Content-Type", "application/json");
+    ajax.send();
+}
+deleteTweet();
+
+
+
+function showTweets() {
+    let ajax = new XMLHttpRequest();
+    ajax.onreadystatechange = function() {
+        if(this.readyState == 4 && this.status == 200) {
+            for(i = 0; i < posts.length; i++) {
+                console.log(JSON.parse(this.responseText));
+                let posts = JSON.parse(this.responseText);
+                let display = document.getElementById('display');
+                display.innerText = posts[0].body;
+            }
+        }
+    }
+    ajax.open("GET", "https://jsonplaceholder.typicode.com/posts", true);
+    ajax.send();
+}
+showTweets();
+
+
+
